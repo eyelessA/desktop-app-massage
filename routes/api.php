b9fc4cb549c3a049ae0657da9086a8057c5281b3
olegbugaev <bugaev0leg@yandex.ru>
2025-03-02 20:30:18 +0100

    feat: Добавит api.

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\TherapistController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/therapists', [TherapistController::class, 'index'])->name('therapists.index');
+
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('users.index');
+    Route::get('/number-of-users-registered-today', [UserController::class, 'numberOfUsersRegisteredToday'])->name('users.numberOfUsersRegisteredToday');
+});
